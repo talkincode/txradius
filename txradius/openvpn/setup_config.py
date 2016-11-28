6e93e16e7f1f63f0a1f402e9ea7d2652b42c3a31
wangjuntao <wjt@toughstruct.com>
2016-11-28 20:52:33 +0800

    1.0.7 -  1.0.6

--- txradius/openvpn/setup_config.py
+++ txradius/openvpn/setup_config.py
@@ -10,9 +10,13 @@ radius_auth_port=18121
 radius_acct_port=18131
 radius_secret=secret
 radius_timeout=3
+acct_interval=60
+session_timeout=864000
 logfile=/var/log/txovpn.log
 statusfile=/etc/openvpn/openvpn-status.log
 statusdb=/etc/openvpn/txovpn.db
+client_config_dir=/etc/openvpn/ccd
+server_manage_addr=127.0.0.1:7505
 '''
 
 def echo():
