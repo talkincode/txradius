178741581964fd24c284fed1bead6da5a076dbbb
wangjuntao <wjt@toughstruct.com>
2016-11-28 15:10:39 +0800

    1.0.5 -  update

--- scripts/txovpn_down.sh
+++ scripts/txovpn_down.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-pgrep -f txovpn_daemon | xargs  kill
-
+if [ $( pgrep -f txovpn_daemon | wc -l ) -gt 0 ];then
+    pgrep -f txovpn_daemon | xargs  kill
+fi
 txovpn_initdb -c /etc/openvpn/txovpn.conf
--- scripts/txovpn_up.sh
+++ scripts/txovpn_up.sh
@@ -4,4 +4,4 @@ echo 'openvpn up'
 
 txovpn_initdb -c /etc/openvpn/txovpn.conf
 
-nohup txovpn_daemon > /dev/null 2>&1
+nohup txovpn_daemon > /dev/null 2>&1 &
